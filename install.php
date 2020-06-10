6eb6ad352aacd2188cfe9484a338f5ed354c99d5
Михайлов Михаил <mixajlov.mikhail92@gmail.com>
2020-06-10 23:13:47 +0300

    fix

--- install.php
+++ install.php
@@ -5,8 +5,6 @@ function install_package()
 	$core   = cmsCore::getInstance();
 	$db = cmsDatabase::getInstance();
 	$is_controller_setup = $core->db->getRow('controllers', 'name="autologin"');
-	var_dump($is_controller_setup);
-	exit();
 	 $config = cmsConfig::getInstance();
 	 $admin  = cmsCore::getController('admin');
 	 $path = $config->upload_path . $admin->installer_upload_path;
