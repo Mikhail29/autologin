3cd843937968ff3fa800a5133874c2011f52f1e7
Михайлов Михаил <mixajlov.mikhail92@gmail.com>
2020-06-10 23:18:31 +0300

    fix

--- install.php
+++ install.php
@@ -10,12 +10,12 @@ function install_package()
 	 $path = $config->upload_path . $admin->installer_upload_path;
 	 if($is_controller_setup)
 	 {
-         if($is_controller_setup["version"] != "2.0.0" && $is_controller_setup["version"] != "2.1.0" {
-             return $db->importDump($path . '/updateto210.sql');
+         if($is_controller_setup["version"] != "2.0.0" && $is_controller_setup["version"] != "2.1.0") {
+             return $db->importDump($path . '/updateto200.sql');
          }
          else if($is_controller_setup["version"] == "2.0.0")
          {
-             return $db->importDump($path . '/updateto200.sql');
+             return $db->importDump($path . '/updateto210.sql');
          }
 	 }
 	 else
