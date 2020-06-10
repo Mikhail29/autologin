dae9b71648ef8eeacb4f6dffcbafdb1f5bd22228
Михайлов Михаил <mixajlov.mikhail92@gmail.com>
2020-06-10 22:39:22 +0300

    fix

---
+++ system/languages/en/controllers/autologin/widgets/fastlogin.php
@@ -0,0 +1,6 @@
+<?php
+
+define('LANG_WD_AUTOLOGIN_FASTLOGIN_LOGIN',     'Sign ');
+define('LANG_WD_AUTOLOGIN_FASTLOGIN_ADD_USER',     'Add to user list ');
+define('LANG_WD_AUTOLOGIN_FASTLOGIN_DELETE_USER',     'Remove from the user list ');
+define('LANG_WD_AUTOLOGIN_FASTLOGIN_HREF_TO_LIST',     'Back to list');
